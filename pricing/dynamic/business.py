45110eda673c86f63ddb08cb0c8469986977ead6
kabir-vats <kabir.vats.2004@gmail.com>
2025-01-20 15:32:55 -0800

    update business

--- pricing/dynamic/business.py
+++ pricing/dynamic/business.py
@@ -8,20 +8,22 @@ class Business:
     def __init__(self, costs: List[float], customer: Customer) -> None:
         self.costs = costs
         self.net_profit = 0
-        self.prices = costs.copy()
         self.tiers = len(costs)
         self.customer = customer
         self.transaction_history = DataFrame(columns=['prices', 'chosen_tier'])
 
-    def sell(self) -> None:
-        tier = self.customer.choose_tier(self.costs, self.prices)
+    def sell(self, prices: List[float]) -> pd.DataFrame:
+        tier = self.customer.choose_tier(self.costs, prices)
         self.net_profit += self.prices[tier] - self.costs[tier]
-        new_transaction = pd.DataFrame([[self.prices, tier]],
+        new_transaction = pd.DataFrame([[prices, tier]],
                                        columns=self.transaction_history.columns)
         self.transaction_history = pd.concat([new_transaction,
                                               self.transaction_history],
                                              ignore_index=True)
+        return new_transaction
 
-    def sell_n(self, n: int) -> None:
+    def sell_n(self, prices: List[float], n: int) -> pd.DataFrame:
+        profit_before = self.net_profit
         for _ in range(n):
-            self.sell()
+            self.sell(prices)
+        return self.net_profit - profit_before, self.transaction_history.tail(n)
