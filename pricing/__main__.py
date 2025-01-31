7738c7905e579a590f7160d15fed36b321a4b8ee
kabir-vats <kabir.vats.2004@gmail.com>
2025-01-30 23:49:38 -0800

    remove unused imports from main

--- pricing/__main__.py
+++ pricing/__main__.py
@@ -1,10 +1,3 @@
-from pricing.static.system import TieredPricingSystem
-from pricing.static.optimize import GradientDescent, DualAnnealing # noqa
-from pricing.util.simulate import simulate_optimal_profits
-from pricing.util.visualize import surface_plot
-import matplotlib.pyplot as plt
-
-
 def main():
     pass
 
